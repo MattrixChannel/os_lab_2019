bd9b59b4ed9ac330bd7b25410855861df2f43737
Petr <mattrixchannel@gmail.com>
2024-09-21 12:22:32 +0000

    Task 2 (in progress1)

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,20 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str);
+//	printf("%i", len);
+	char buf;
+
+	printf("%s\n", str);
+
+	for (int i = 0; i < len / 2; i++) {
+		buf = str[i];
+		str[i] = str[len - i];
+		str[len - i] = buf;
+	}
+	printf("%s\n", str);
 }
 
